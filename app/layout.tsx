077b6b078cd9b94576e4c9ec40c4958010986df3
FrancoSellan <francoesellan@gmail.com>
2026-03-21 19:42:02 -0300

    feat(analytics): :arrow_up: agrego analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Oswald } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const oswald = Oswald({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={` ${oswald.className} bg-[#1a1a1a] text-gray-300`}>
+        <Analytics />
         {children}
       </body>
     </html>
